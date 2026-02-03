61d2fd95097a669982d5d47895f719cc1806b200
emkuwa <84395152+emkuwa@users.noreply.github.com>
2026-02-03 06:28:07 +0300

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -39,15 +39,14 @@ Guest message:
       "Welcome to Zanzibar 🌴 How can I help you plan your trip?";
 
     return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ reply })
-    };
+  statusCode: 200,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  },
+  body: JSON.stringify({ reply })
+};
 
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
   }
 }
